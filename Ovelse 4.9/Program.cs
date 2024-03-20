ba2c6f06ec8460bbbfbc8a9705c51a3c3ccd9d7f
Christoffer Olsen <chris70944@gmail.com>
2024-03-20 22:27:49 +0100

    Add project files.

---
+++ Ovelse 4.9/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hvad vil du have at drikke?");
+string drik = Console.ReadLine();
+
+if (String.Equals(drik, "Kaffe", StringComparison.OrdinalIgnoreCase))
+{
+    Console.WriteLine("Det er et godt valg");
+}
+else
+{
+    Console.WriteLine("Det er også  okay");
+}
